b4cf32e3d4bcaa280ef0b9f41cb004dc76c118c3
Bastiaan Zapf <bzapf@online.de>
2013-09-10 17:52:53 +0200

    Mehr Ideogramme

--- script/ueberschrift-interaktiv.py
+++ script/ueberschrift-interaktiv.py
@@ -10,11 +10,38 @@ ideo = valueDialog('Piraten OpenAntrag Flyer',
 """
 Ideogramm:
 Copy&Paste ODER
-1 für "Personen"
+1 für "Personen/Parlament"
+2 für "Home"
+3 für "Balkendiagramm"
+4 für "Lupe"
+5 für "Schildchen"
+6 für "RSS/Rundfunk"
+7 für "Netzwerk/Weltkugel"
+8 für "Fragezeichen"
+9 für "Megaphon"
+10 für "Info"
 """)
 
 if (ideo=="1"):
     ideo=""
+elif (ideo=="2"):
+    ideo=""
+elif (ideo=="3"):
+    ideo=""
+elif (ideo=="4"):
+    ideo=""
+elif (ideo=="5"):
+    ideo=""
+elif (ideo=="6"):
+    ideo=""
+elif (ideo=="7"):
+    ideo=""
+elif (ideo=="8"):
+    ideo=""
+elif (ideo=="9"):
+    ideo=""
+elif (ideo=="10"):
+    ideo=""
 
 text = valueDialog('Piraten OpenAntrag Flyer','Text')
 
